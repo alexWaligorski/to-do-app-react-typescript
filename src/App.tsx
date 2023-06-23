f958517476f1829e3eb75af618665dd6cfa50560
Alexandra Waligorski <waligorski.alexandra@gmail.com>
2023-06-23 12:46:02 +0200

    add state to input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "./components/InputField";
 
 import "./App.css";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
   return (
     <div className="App">
       <h1>Task Manager</h1>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./styles.css";
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
       <input
         type="input"
         placeholder="Enter a new task"
         className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button type="submit" className="input_submit">
         GO
